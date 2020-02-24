9ff49560782aff3cbc0b1278e6588e6dd538ecfe
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-02-24 15:16:29 -0400

    Modelo Pagos creado

---
+++ Tarea4_SistemadeInscripcion/Models/Pagos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tarea4_SistemadeInscripcion.Models
+{
+    public class Pagos
+    {
+        [Key]
+        public int PagoId { get; set; }
+        public DateTime Fecha { get; set; }
+        public int InscripcionId { get; set; }
+        public float Monto { get; set; }
+
+        public Pagos()
+        {
+            PagoId = 0;
+            Fecha = DateTime.Now;
+            InscripcionId = 0;
+            Monto = 0;
+        }
+    }
+}
