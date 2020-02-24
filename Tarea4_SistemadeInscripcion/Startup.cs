79ce2e563be3757354fca15ceb53c7ee57667adf
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-02-24 14:52:46 -0400

    Elementos existentes necesarios editados!

--- Tarea4_SistemadeInscripcion/Startup.cs
+++ Tarea4_SistemadeInscripcion/Startup.cs
@@ -9,7 +9,6 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Tarea4_SistemadeInscripcion.Data;
 
 namespace Tarea4_SistemadeInscripcion
 {
