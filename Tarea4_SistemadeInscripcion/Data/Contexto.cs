fbea7044080549c0e787bd9df4c7e1297f1bfc18
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-02-24 15:24:22 -0400

    Contexto creado y modelos agregados al contexto!

---
+++ Tarea4_SistemadeInscripcion/Data/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tarea4_SistemadeInscripcion.Models;
+
+namespace Tarea4_SistemadeInscripcion.Data
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Asignaturas> Asignaturas { get; set; }
+        public DbSet<Estudiantes> Estudiantes { get; set; }
+        public DbSet<Inscripciones> Inscripciones { get; set; }
+        public DbSet<Pagos> Pagos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = Database/Tarea4.Db");
+        }
+    }
+}
